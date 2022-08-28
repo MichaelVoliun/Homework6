e5686785bd4b8f0ffa48b20d58314a767211f562
MichaelVoiun <supermishka3@gmail.com>
2022-08-28 19:12:55 +0300

    Первый коммит

---
+++ src/Main.java
@@ -0,0 +1,6 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Домашнее задание по теме 1.5 Массивы чсать 2");
+        System.out.println();
+    }
+}
